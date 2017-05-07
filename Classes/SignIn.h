442d8294d9262ac8fd7a085c653e8dfed09b773d
zhenzhenz <zhenzhenz@users.noreply.github.com>
2017-05-07 11:28:05 +0800

    Room界面
    补充了登录成功回显功能。
    添加了选择地图创建房间页面

--- Classes/SignIn.h
+++ Classes/SignIn.h
@@ -2,42 +2,43 @@
 #define __SIGN_IN_H__
 
 #include "cocos2d.h"
-#include"cocos-ext.h"
 #include"StartScene.h"
 
-USING_NS_CC_EXT;
 
 class SignIn : public cocos2d::Layer, public cocos2d::TextFieldDelegate
 {
 public:
 	static cocos2d::Scene* createScene();
 
-	virtual bool init();
+	virtual bool init();  
+	//添加对单击输入框事件的监听
 	virtual void onEnter();
+	//清除监听
 	virtual void onExit();
-
+	
+	//处理对单击用户名密码文本框的监听
 	bool touchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
 	void touchMoved(cocos2d::Touch *touch, cocos2d::Event *event);
 	void touchEnded(cocos2d::Touch *touch, cocos2d::Event *event);
 
-	// 当用户启动虚拟键盘的时候回调函数
+	//控制用户名密码输入过程的回调函数
 	virtual bool onTextFieldAttachWithIME(TextFieldTTF * sender);
-	// 当用户关闭虚拟键盘的时候回调函数
 	virtual bool onTextFieldDetachWithIME(TextFieldTTF * sender);
-	// 当用户进行输入 虚拟键盘的时候回调函数
 	virtual bool onTextFieldInsertText(TextFieldTTF * sender, const char * text, size_t nLen);
-	// 当用户进行删除文字 虚拟键盘的时候回调函数
 	virtual bool onTextFieldDeleteBackward(TextFieldTTF * sender, const char * delText, size_t nLen);
 
-	void ClickInCall(Ref* obj);
-	void ClickUpCall(Ref* obj);
+	//signin，signup的点击回调函数 
+	void clickInCall(Ref* obj);
+	void clickUpCall(Ref* obj);
 
 	CREATE_FUNC(SignIn);
+	//设置指向上级startscene的指针
+	void setStartSce(StartScene*);
 
 private:
-	TextFieldTTF* _Name;
-	TextFieldTTF* _Password;
-
+	TextFieldTTF* _Name;  //用户名
+	TextFieldTTF* _Password;  //密码
+	StartScene* _startSce = nullptr;  //指向上级startscene的指针
 };
 
-#endif /
+#endif 
